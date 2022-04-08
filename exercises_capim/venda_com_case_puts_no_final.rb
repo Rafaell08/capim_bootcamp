20c7bae138992a50bf73bc4f652464c3e774ea18
Rafael <rafael.cavassana@capim.com.br>
2022-04-08 15:36:18 -0300

    Add venda com case e puts apenas no final

---
+++ exercises_capim/venda_com_case_puts_no_final.rb
@@ -0,0 +1,32 @@
+=begin
+    Venda de Livros: Tenho 1 livro de 29.90, quero saber o valor se eu vender com desconto de 50% ou 25% ou 5%. Fazer duas versões, uma usando o if e outra o case.
+=end
+
+# case
+
+puts '1 - Desconto de 50%'
+puts '2 - Desconto de 25%'
+puts '3 - Desconto de 5%'
+print 'Escolha uma das opções de desconto: '
+
+calc = gets.chomp.to_i
+
+vfinal = 'O valor com desconto é: '
+
+a = 29.90
+x = (a * 0.5) 
+y = (a * 0.25)
+z = (a * 0.05)
+
+case calc
+when 1
+    result = a - x
+when 2
+    result = a - y
+when 3
+    result = a - z
+else
+    puts 'Opção inválida.'
+end
+
+puts "#{vfinal} #{result}"
