80c32627e91c36259593ed43e26a73bdabafde3a
Rafael <rafael.cavassana@capim.com.br>
2022-04-11 16:26:51 -0300

    Add imprimir maior valor e chave no final

--- exercises_ruby_course/hash_imprimir_maior_valor_no_final.rb
+++ exercises_ruby_course/hash_imprimir_maior_valor_no_final.rb
@@ -4,6 +4,8 @@ numbers = {A: 10, B: 30, C: 20, D: 25, E: 15}
 puts "Selecionando o maior valor para imprimir junto com a chave."
 
 greather_value = numbers.select do |key, value|
+    puts "Key: #{key}"
+    puts "Value: #{value}"
     value == numbers.values.max
 end
 
