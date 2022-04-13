6cd2a80a98527d455e95eab0e9da20e017351513
Rafael <rafaelcavassana@gmail.com>
2022-04-13 16:05:48 -0300

    Add venda de livros com array

---
+++ exercises_capim/venda_de_livros_arrays.rb
@@ -0,0 +1,15 @@
+
+a = 29.90
+book1 = a - (a * 0.45)
+book2 = a - (a * 0.3)
+book3 = a
+
+books = ["Livro 1", "Livro 2", "Livro 3"]
+orders = []
+
+puts "Aqui temos três livros a venda: #{books}"
+
+books.each do |venda|
+    
+    puts "#{book1_sold}"
+end
