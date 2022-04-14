03baa522d3be2a2b49342f7fd0f4da03e926aba4
Rafael <rafaelcavassana@gmail.com>
2022-04-14 15:39:14 -0300

    Fix vendas de livros com arrays

--- exercises_capim/venda_de_livros_arrays.rb
+++ exercises_capim/venda_de_livros_arrays.rb
@@ -1,38 +1,21 @@
-=begin
 
-a = 29.90
-book1 = a - (a * 0.45)
-book2 = a - (a * 0.3)
-book3 = a
-
-books = ["Livro 1", "Livro 2", "Livro 3"]
-orders = []
-
-puts "Aqui temos três livros a venda: #{books}"
-
-books.each do |venda|
-    
-    puts "#{book1_sold}"
-end
-=end
-
-books = ["Odisséia - Homero", "Hamlet – William Shakespeare", "Guerra e Paz – Leon Tolstoi", "Morte e Vida Severina - João Cabral de Melo Neto"]
+books = ['Odisséia - Homero', 'Hamlet – William Shakespeare', 'Guerra e Paz – Leon Tolstoi', 'Morte e Vida Severina - João Cabral de Melo Neto']
 orders = []
 
 puts "Temos quatro livros: #{books}"
 puts "Qual dos livros acima você deseja vender?"
 puts "Insira os livros que serão vendidos: "
+get0 = gets.chomp
 get1 = gets.chomp
 get2 = gets.chomp
-get3 = gets.chomp
 
-# a = 29.90
-# x = 29.90 - ( a * 0.45 )
-# y = 29.90 - ( a * 0.30 )
+a = 29.90
+x = 29.90 - ( a * 0.45 )
+y = 29.90 - ( a * 0.30 )
 
-orders.push(get1, get2, get3)
-books.delete_if { |books| books.to_s.start_with?(get1, get2, get3) }
-# books.delete_if { |books| books.to_s.include?(get) }
-    
-puts "O livro vendido foi: #{orders}"
-puts "Livros restantes: #{books}"
+orders.push(get0, get1, get2)
+books.delete_if { |books| books.to_s.start_with?(get0, get1, get2) }
+
+puts "O primeiro livro foi: #{orders.to_s[get0]}; Seu valor original é: R$#{a} e com desconto ficou R$#{x.round(2)}."
+puts "O segundo livro foi: #{orders.to_s[get1]}; Seu valor original é: R$#{a} e com desconto ficou R$#{y.round(2)}."
+puts "O terceiro livro foi: #{orders.to_s[get2]}; Seu valor original é: R$#{a} mas o teceiro não tem desconto. =("
