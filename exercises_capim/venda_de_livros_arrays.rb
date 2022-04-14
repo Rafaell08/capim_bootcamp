2f9c2bfb890455d81e85434ad7fe43a4a234e6d4
Rafael <rafaelcavassana@gmail.com>
2022-04-14 16:03:16 -0300

    Fix vendas de livros com arrays

--- exercises_capim/venda_de_livros_arrays.rb
+++ exercises_capim/venda_de_livros_arrays.rb
@@ -4,17 +4,21 @@ orders = []
 
 puts "Temos quatro livros: #{books}"
 puts "Qual dos livros acima você deseja vender?"
-puts "Insira os livros que serão vendidos: "
+print "Livro 1: "
 get0 = gets.chomp
+print "Livro 2: "
 get1 = gets.chomp
+print "Livro 3: "
 get2 = gets.chomp
+
+puts "\n--------------"
 
 a = 29.90
 x = 29.90 - ( a * 0.45 )
 y = 29.90 - ( a * 0.30 )
 
 orders.push(get0, get1, get2)
-books.delete_if { |books| books.to_s.start_with?(get0, get1, get2) }
+books.delete_if { |books| books.start_with?(get0, get1, get2) }
 
 puts "O primeiro livro foi: #{orders.to_s[get0]}, seu valor original é R$#{a} o desconto foi de 45% e o valor final foi R$#{x.round(2)}."
 puts "O segundo livro foi: #{orders.to_s[get1]}, seu valor original é R$#{a} o desconto foi de 30% e o valor final foi R$#{y.round(2)}."
