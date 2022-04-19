4b45db688dcb687477000aac7a730199bff0445d
Rafael <rafaelcavassana@gmail.com>
2022-04-19 16:34:06 -0300

    Add verificando se o cpf ou cnpj informado verdadeiro

---
+++ exercises_capim/validade_cpf_or_cnpj.rb
@@ -0,0 +1,37 @@
+require "cpf_cnpj"
+
+def valid_cpf
+  print "Insira CPF: "
+  cpf = gets.chomp
+
+  if CPF.valid?("#{cpf}")
+    puts "O CPF é verdadeiro!"
+  else
+    puts "O CPF é falso!"
+  end
+end
+
+def valid_cnpj
+  print "Insira CNPJ: "
+  cnpj = gets.chomp
+
+  if CNPJ.valid?("#{cnpj}")
+    puts "O CNPJ é verdadeiro!"
+  else
+    puts "O CNPJ é falso!"
+  end
+end
+
+puts "Deseja consultar CPF ou CNPJ?"
+puts "1 - CPF"
+puts "2 - CNPJ"
+print "Opção: "
+
+option = gets.chomp.to_i
+
+case option
+when 1
+  valid_cpf
+when 2
+  valid_cnpj
+end
