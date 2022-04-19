51f698e32b44af7d26052b87039431b1b3c30376
Rafael <rafaelcavassana@gmail.com>
2022-04-19 11:16:23 -0300

    Fix ajustado com boas práticas de programação

--- exercises_capim/venda_de_livros_arrays.rb
+++ exercises_capim/venda_de_livros_arrays.rb
@@ -13,13 +13,17 @@ get2 = gets.chomp
 
 puts "\n--------------"
 
-a = 29.90
-x = 29.90 - ( a * 0.45 )
-y = 29.90 - ( a * 0.30 )
+price = 29.90
+first_sale_price = 29.90 - ( price * 0.45 )
+second_sale_price = 29.90 - ( price * 0.30 )
 
 orders.push(get0, get1, get2)
 books.delete_if { |books| books.start_with?(get0, get1, get2) }
 
-puts "O primeiro livro foi: #{orders.to_s[get0]}, seu valor original é R$#{a} o desconto foi de 45% e o valor final foi R$#{x.round(2)}."
-puts "O segundo livro foi: #{orders.to_s[get1]}, seu valor original é R$#{a} o desconto foi de 30% e o valor final foi R$#{y.round(2)}."
-puts "O terceiro livro foi: #{orders.to_s[get2]}, seu valor original é R$#{a} mas o teceiro não tem desconto. =("
+puts "O primeiro livro foi: #{orders.to_s[get0]}, seu valor original é R$#{price} o desconto foi de 45% e o valor final foi R$#{first_sale_price.round(2)}."
+puts "O segundo livro foi: #{orders.to_s[get1]}, seu valor original é R$#{price} o desconto foi de 30% e o valor final foi R$#{second_sale_price.round(2)}."
+puts "O terceiro livro foi: #{orders.to_s[get2]}, seu valor original é R$#{price} mas o teceiro não tem desconto. =("
+
+puts "\n--------------"
+
+puts "Aqui temos os livros vendidos: #{orders}\nAqui temos os livros restantes: #{books}"
