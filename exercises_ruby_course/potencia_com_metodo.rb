e28f94193762aafb27b245b89e6847fc86859a9e
Rafael <rafaelcavassana@gmail.com>
2022-04-20 14:32:34 -0300

    Fix alteração feita no metodo

--- exercises_ruby_course/potencia_com_metodo.rb
+++ exercises_ruby_course/potencia_com_metodo.rb
@@ -1,16 +1,15 @@
 
-def potency
-  puts "Digite a base e o expoente desejado:"
-
-  print "Base: "
-  base = gets.chomp.to_f
-  print "Expoente: "
-  exponent = gets.chomp.to_f
-
+def potency(base, exponent)
   result = base ** exponent
 
   puts "O resultado da potência é: #{result}"
-
 end
 
-potency
+puts "Digite a base e o expoente desejado:"
+
+print "Base: "
+base = gets.chomp.to_f
+print "Expoente: "
+exponent = gets.chomp.to_f
+
+potency(base, exponent)
