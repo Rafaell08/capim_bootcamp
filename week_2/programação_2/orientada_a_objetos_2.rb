04d2f518215015cb5cd823b7b3e878aa78cd3d68
Rafael <rafaelcavassana@gmail.com>
2022-04-26 18:29:10 -0300

    Add programação orientada a objetos 2

---
+++ week_2/programação_2/orientada_a_objetos_2.rb
@@ -0,0 +1,5 @@
+require './programacao_2' # Funciona mas leva em conta onde você está (pasta).
+# require_relative 'programacao_2' # Leva em conta onde o aplicativo está sendo executado.
+
+animal = Animal.new
+animal.pular
